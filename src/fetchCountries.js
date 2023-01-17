128449af9757463b782716dccf89873caf0403fb
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-17 19:54:12 +0200

    use fetchCountries

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,2 +1,20 @@
 let countryNameURL = 'https://restcountries.com/v3.1/name'
-let filterfeatures =  'namel,capital,population,flags,languages'
+let filterfeatures = 'namel,capital,population,flags,languages'
+
+function fetchCountries(name) {
+    return fetch(`${countryNameURL}${name}?fields=${filterfeatures}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        return response.json();
+    });
+}
+
+// fetch('http://example.com/movies.json')
+//   .then(response => {
+//     return response.json();
+//   })
+
+
+  export default fetchCountries;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+
 
 const DEBOUNCE_DELAY = 300;
