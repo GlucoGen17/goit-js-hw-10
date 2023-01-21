363476f7da056dd27178e42db06bc875182d5d24
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-21 14:49:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import debounce from 'lodash.debounce';
 
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector("search-box");
+const countryList = document.querySelector('country-list');
+const countryInfo = document.querySelector('country-info');
+
+searchBox.addEventListener('input', debounce(s))
