b965d490b6eebf55802137b20653a9491840fe00
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-21 16:49:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,30 @@ import debounce from 'lodash.debounce';
 
 
 const DEBOUNCE_DELAY = 300;
-const searchBox = document.querySelector("search-box");
+const searchBox = document.querySelector("#search-box");
 const countryList = document.querySelector('country-list');
 const countryInfo = document.querySelector('country-info');
 
-searchBox.addEventListener('input', debounce ( e => {
+searchBox.addEventListener('input', debounce(e => {
     const name = searchBox.value.trim();
     if (!name) {
         createPage();
         return;
-    };
-    fetchCountries(name)
-        .then()
-        .catch()
+    } else {
+        fetchCountries(name)
+            .then(nameData)
+            .catch(nameError)
+    }
 }, DEBOUNCE_DELAY))
+
+function nameData() {
+    
+}
+
+function nameError() {
+
+}
+
+// Функція виводу інформації про країни в назві яких є символи, що ми передираємо
+
+// Функція виводу інформації про країну в назві якої всі символи повністю збігаються
