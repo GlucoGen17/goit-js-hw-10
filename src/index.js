6cc53bfb3f96688899d510bb23bcb88acab64491
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-23 21:50:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,52 +3,50 @@ import Notiflix from 'notiflix';
 // import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
 
-
 const DEBOUNCE_DELAY = 300;
-const searchBox = document.querySelector("#search-box");
+const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('country-list');
 const countryInfo = document.querySelector('country-info');
 
-searchBox.addEventListener('input', debounce(e => {
+searchBox.addEventListener(
+  'input',
+  debounce(e => {
     const name = e.target.value.trim();
     if (!name) {
-        clearPage();
-        return;
+      clearPage();
+      return;
     } else {
-        fetchCountries(name)
-            .then(nameData)
-            .catch(nameError)
-    }
-}, DEBOUNCE_DELAY))
-// функція порівняння назв за їх складовою частиною. назва === назві то виводимо інформацію, якщо частина назви === частині то виводимо список де є ці частини
-function nameData() {
-    clearPage();
-    if (country.length > 10) {
-        Notiflix.Notify.info(
-            'Too many matches found. Please enter a more specific name.');
+      fetchCountries(name)
+        .then(country => {
+          clearPage();
+          if (country.length > 10) {
+            Notiflix.Notify.info(
+              'Too many matches found. Please enter a more specific name.'
+            );
             return;
-    } else if (country.length >= 2 && country.length <= 10) {
-        createLIstCountry(country);
-    } else if (country.length === 1) {
-        createInfoCountry(country);
+          } else if (country.length >= 2 && country.length <= 10) {
+            countryList.innerHTML = createLIstCountry(country);
+          } else if (country.length === 1) {
+            countryInfo.innerHTML = createInfoCountry(country);
+          }
+        })
+        .catch(error => {
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+          clearPage();
+          return error;
+        });
     }
-}
-function nameError(error) {
-Notiflix.Notify.failure('Oops, there is no country with that name');
-clearPage();
-return error;
-}
+  }, DEBOUNCE_DELAY)
+);
 
 // Функція виводу інформації про країни в назві яких є символи, що ми передираємо
 function createLIstCountry(country) {
-const markup = country
-  .map(
-    ({ name, flags }) => `<li>
+  const markup = country
+    .map(
+      ({ name, flags }) => `<li>
         <img src="${flags.svg}" alt="${name}" width="30", height="15">&nbsp${name.official}`
-  )
-  .join('');
+    )
+    .join('');
 }
 // Функція виводу інформації про країну в назві якої всі символи повністю збігаються
-function createInfoCountry(country) {
-
-}
+function createInfoCountry(country) {}
